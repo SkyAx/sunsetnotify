594892dd29020915768d539a79f45235685d9850
sk_ax <alxndr.konkov@gmail.com>
2019-02-18 14:30:45 +0300

    Added moment and date functionality

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const cmd = require('node-cmd');
 const cron = require('node-cron');
 const got = require('got');
+const moment = require('moment');
 
 const apiKey = 'b6907d289e10d714a6e88b30761fae22';
 const cityId = '620127';
 const url = `https://openweathermap.org/data/2.5/weather?id=${cityId}&appid=${apiKey}`;
-let sunsetTime;
+
+moment().format();
 
 const getSunsetTime = () => {
-    got(url, { json: true })
+    return got(url, { json: true })
         .then(response => sunsetTime = JSON.stringify(response.body.sys.sunset))
         .catch(error => console.log(error.response.body));
-    return sunsetTime;
 };
 
-const task = cron.schedule('* * * * * *', () => {
+const timeToGo = () => {
+    let hour, minutes;
+
+    getSunsetTime().then((res) => {
+        let sunset = moment.unix(res);
+        hour = sunset.hour();
+        minutes = sunset.minute();
+    });
+};
+
+
+const task = cron.schedule(`* * * * * *`, () => {
 });

11643798ee0419b1d81e4a7d762611e7fc543d2f
sk_ax <alxndr.konkov@gmail.com>
2019-02-18 10:11:06 +0300

    Added args to buzzer

--- buzzer.py
+++ buzzer.py
@@ -1,15 +1,38 @@
 import RPi.GPIO as GPIO
+import sys, getopt
 from time import sleep
+
+def main(argv):
+   count = 0
+   delay = 0
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["count=","delay="])
+   except getopt.GetoptError:
+      print 'buzzer.py -c <count> -d <delay>'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'buzzer.py -c <count> -d <delay>'
+         sys.exit()
+      elif opt in ("-c", "--cnt"):
+         count = arg
+      elif opt in ("-d", "--dly"):
+         delay = arg
+   print 'Count of buzz is "', count
+   print 'Delay of buzz is "', delay
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 buzzer=23
-count=8
 GPIO.setup(buzzer,GPIO.OUT)
 while count >= 0:
         GPIO.output(buzzer,GPIO.HIGH)
-        sleep(0.5)
+        sleep(delay)
         GPIO.output(buzzer,GPIO.LOW)
-        sleep(0.5)
+        sleep(delay)
         count -= 1
 GPIO.output(buzzer,GPIO.LOW)
-sleep(0.5)
+sleep(1)

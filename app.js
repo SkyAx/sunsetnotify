c41ebe42466d51bfbf3447b86b1c772a9b078639
sk_ax <alxndr.konkov@gmail.com>
2019-02-18 18:38:14 +0300

    Created notify and sunset cheer

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const apiKey = 'b6907d289e10d714a6e88b30761fae22';
 const cityId = '620127';
 const url = `https://openweathermap.org/data/2.5/weather?id=${cityId}&appid=${apiKey}`;
 
+let sunsetHour = 0;
+let sunsetMinutes = 0;
+
 moment().format();
 
 const getSunsetTime = () => {
@@ -16,15 +19,21 @@ const getSunsetTime = () => {
 };
 
 const timeToGo = () => {
-    let hour, minutes;
-
     getSunsetTime().then((res) => {
         let sunset = moment.unix(res);
-        hour = sunset.hour();
-        minutes = sunset.minute();
+        sunsetHour = sunset.hour() - 1;
+        sunsetMinutes = sunset.minute();
     });
 };
 
+timeToGo();
 
-const task = cron.schedule(`* * * * * *`, () => {
+const checkSunset = cron.schedule(`0 0 9 * * *`, () => {
+    timeToGo();
+});
+
+const nofity = cron.schedule(`0 ${sunsetMinutes} ${sunsetHour} * * *`, () =>{
+    cmd.get('python buzzer -c 7 -d 0.5', () => {
+        console.log('Ok')
+    });
 });
